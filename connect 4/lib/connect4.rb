f5689323143d762c0ec7ec10923a8e505552f9f6
Hunter-Kendall <hunter.kendall23@houghton.edu>
2023-08-07 10:21:52 -0400

    finished check win and ready for refactoring

---
+++ connect 4/lib/connect4.rb
@@ -0,0 +1,140 @@
+class Board
+    attr_accessor :board
+    def initialize
+        @board = [[" _ ", " _ ", " _ ", " _ ", " _ ", " _ ", " _ "],
+                  [" _ ", " _ ", " _ ", " _ ", " _ ", " _ ", " _ "],
+                  [" _ ", " _ ", " _ ", " _ ", " _ ", " _ ", " _ "],
+                  [" _ ", " _ ", " _ ", " _ ", " _ ", " _ ", " _ "],
+                  [" _ ", " _ ", " _ ", " _ ", " _ ", " _ ", " _ "],
+                  [" _ ", " _ ", " _ ", " _ ", " _ ", " _ ", " _ "]]
+    end
+    def board_reset
+        @board = [[" _ ", " _ ", " _ ", " _ ", " _ ", " _ ", " _ "],
+                 [" _ ", " _ ", " _ ", " _ ", " _ ", " _ ", " _ "],
+                 [" _ ", " _ ", " _ ", " _ ", " _ ", " _ ", " _ "],
+                 [" _ ", " _ ", " _ ", " _ ", " _ ", " _ ", " _ "],
+                 [" _ ", " _ ", " _ ", " _ ", " _ ", " _ ", " _ "],
+                 [" _ ", " _ ", " _ ", " _ ", " _ ", " _ ", " _ "]]
+    end
+    def board_display
+        puts"""
+        |#{@board[5][0]}|#{@board[5][1]}|#{@board[5][2]}|#{@board[5][3]}|#{@board[5][4]}|#{@board[5][5]}|#{@board[5][6]}|\n
+        |#{@board[4][0]}|#{@board[4][1]}|#{@board[4][2]}|#{@board[4][3]}|#{@board[4][4]}|#{@board[4][5]}|#{@board[4][6]}|\n
+        |#{@board[3][0]}|#{@board[3][1]}|#{@board[3][2]}|#{@board[3][3]}|#{@board[3][4]}|#{@board[3][5]}|#{@board[3][6]}|\n
+        |#{@board[2][0]}|#{@board[2][1]}|#{@board[2][2]}|#{@board[2][3]}|#{@board[2][4]}|#{@board[2][5]}|#{@board[2][6]}|\n
+        |#{@board[1][0]}|#{@board[1][1]}|#{@board[1][2]}|#{@board[1][3]}|#{@board[1][4]}|#{@board[1][5]}|#{@board[1][6]}|\n
+        |#{@board[0][0]}|#{@board[0][1]}|#{@board[0][2]}|#{@board[0][3]}|#{@board[0][4]}|#{@board[0][5]}|#{@board[0][6]}|\n
+        | 1 | 2 | 3 | 4 | 5 | 6 | 7 |
+        """
+    end
+    def place_piece(column, marker)
+        col = column - 1
+        @board.each_with_index do |row, row_index|
+            if @board[row_index][col] == " _ "
+                
+                @board[row_index][col] = " #{marker} "
+                break
+            end
+        end
+    end
+    def check_win(marker)
+        win_string = marker + marker + marker + marker
+        #checks rows
+        @board.each do |row|
+            no_spaces = row.map{ |str| str.gsub(" ", "") }
+            result = no_spaces.join("")
+            if result.match(win_string)
+                return true
+            end
+        end
+        #checks columns
+        (0..6).each do |col|
+            column = [@board[0][col], @board[1][col],@board[2][col],@board[3][col],@board[4][col],@board[5][col]]
+            no_spaces = column.map{ |str| str.gsub(" ", "") }
+            result = no_spaces.join("")
+            if result.match(win_string)
+                return true
+            end
+        end
+        #checks right diagonals from the top 
+        (0..3).each do |start_col|
+            row = 0
+            arr = []
+            (0..6).each do |col_adder|
+                if row <= 5
+                    if start_col + col_adder <= 6
+                        column = start_col + col_adder  # Fixed the typo here
+                        
+                        arr << @board[row][column]
+                        row += 1
+                    end
+                end
+            end
+            no_spaces = arr.map{ |str| str.gsub(" ", "") }
+            result = no_spaces.join("")
+            if result.match(win_string)
+                return true
+            end
+        end
+
+        #checks right diagonals from the left side
+        max_col=6 #starts one higher that it will see
+        (1..2).each do |start_row|
+            max_col -= 1
+            arr = []
+            row = 0 + start_row
+            (0..5).each do |column|
+                # puts "arr: #{arr} row: #{row} col: #{column}"
+                if row <= 5
+                    if column <= max_col
+                        arr << @board[row][column]
+                        row += 1
+                    end
+                end
+            end
+            no_spaces = arr.map{ |str| str.gsub(" ", "") }
+            result = no_spaces.join("")
+            if result.match(win_string)
+                return true
+            end
+        end
+
+
+        return false 
+    end
+end
+class Player
+    attr_accessor :marker, :name
+    def initialize(name)
+        @name = name
+        @marker = "X"
+    end
+    def update_marker(x)
+        @marker = x
+    end
+end
+
+class Game
+    def initialize(board, p1, p2)
+        @board = board
+        @p1 = p1
+        @p2 = p2
+        @p2.update_marker("O")
+        @turn = 0
+        @current_turn = @p1
+    end
+end
+if __FILE__ == $0
+    board = Board.new
+    board.board_display
+    board.board = [[" _ ", " _ ", " _ ", " X ", " X ", " _ ", " _ "],
+            [" X ", " _ ", " _ ", " _ ", " _ ", " _ ", " _ "],
+            [" _ ", " _ ", " _ ", " _ ", " _ ", " _ ", " _ "],
+            [" _ ", " _ ", " _ ", " _ ", " _ ", " _ ", " _ "],
+            [" _ ", " _ ", " _ ", " _ ", " _ ", " _ ", " _ "],
+            [" _ ", " _ ", " _ ", " _ ", " _ ", " _ ", " _ "]]
+    board.check_win("X")
+    p1 = Player.new("hunter")
+    p2 = Player.new("bot")
+    g = Game.new(board, p1, p2)
+end
---
+++ connect 4/spec/conect4_spec.rb
@@ -0,0 +1,144 @@
+require_relative '../lib/connect4'
+
+describe Board do
+    describe "#reset_board"do
+        it "expect board to reset" do
+            board = Board.new
+            board.board = [["x", "sdadf", " _ ", " _ ", " _ ", " _ ", " _ "],
+            [" _ ", " _ ", " _ ", " _ ", " _ ", " _ ", " _ "],
+            [" _ ", " _ ", " _ ", " _ ", " _ ", " _ ", " _ "],
+            [" _ ", " _ ", " _ ", " _ ", " _ ", " _ ", " _ "],
+            [" _ ", " _ ", " _ ", " _ ", " _ ", " _ ", " _ "],
+            [" _ ", " _ ", " _ ", " _ ", "adf", " _ ", " _ "]]  # Make some changes to the board
+            board.board_reset
+            expect(board.board).to eq([[" _ ", " _ ", " _ ", " _ ", " _ ", " _ ", " _ "],
+                [" _ ", " _ ", " _ ", " _ ", " _ ", " _ ", " _ "],
+                [" _ ", " _ ", " _ ", " _ ", " _ ", " _ ", " _ "],
+                [" _ ", " _ ", " _ ", " _ ", " _ ", " _ ", " _ "],
+                [" _ ", " _ ", " _ ", " _ ", " _ ", " _ ", " _ "],
+                [" _ ", " _ ", " _ ", " _ ", " _ ", " _ ", " _ "]]) 
+        end
+    end
+    describe "#place_piece" do
+        context "places piece on desired column." do
+            it "column has no values" do
+                board = Board.new
+                board.place_piece(1, "X")
+                expect(board.board).to eq([[" X ", " _ ", " _ ", " _ ", " _ ", " _ ", " _ "],
+                    [" _ ", " _ ", " _ ", " _ ", " _ ", " _ ", " _ "],
+                    [" _ ", " _ ", " _ ", " _ ", " _ ", " _ ", " _ "],
+                    [" _ ", " _ ", " _ ", " _ ", " _ ", " _ ", " _ "],
+                    [" _ ", " _ ", " _ ", " _ ", " _ ", " _ ", " _ "],
+                    [" _ ", " _ ", " _ ", " _ ", " _ ", " _ ", " _ "]])
+            end
+            it "column has no values" do
+                board = Board.new
+                board.place_piece(1, "X")
+                board.place_piece(1, "X")
+                expect(board.board).to eq([[" X ", " _ ", " _ ", " _ ", " _ ", " _ ", " _ "],
+                    [" X ", " _ ", " _ ", " _ ", " _ ", " _ ", " _ "],
+                    [" _ ", " _ ", " _ ", " _ ", " _ ", " _ ", " _ "],
+                    [" _ ", " _ ", " _ ", " _ ", " _ ", " _ ", " _ "],
+                    [" _ ", " _ ", " _ ", " _ ", " _ ", " _ ", " _ "],
+                    [" _ ", " _ ", " _ ", " _ ", " _ ", " _ ", " _ "]])
+            end
+        end
+    end
+    describe "#check_win" do
+        it "Row win"do
+
+            board = Board.new
+            board.board = [[" _ ", " X ", " X ", " X ", " X ", " X ", " _ "],
+            [" X ", " _ ", " _ ", " _ ", " _ ", " _ ", " _ "],
+            [" _ ", " _ ", " _ ", " _ ", " _ ", " _ ", " _ "],
+            [" _ ", " _ ", " _ ", " _ ", " _ ", " _ ", " _ "],
+            [" _ ", " _ ", " _ ", " _ ", " _ ", " _ ", " _ "],
+            [" _ ", " _ ", " _ ", " _ ", " _ ", " _ ", " _ "]]
+            result = board.check_win("X")
+            expect(result).to be true
+        end
+        it "Row win second row" do
+
+            board = Board.new
+            board.board = [[" _ ", " X ", " X ", " X ", " _ ", " _ ", " _ "],
+            [" X ", " X ", " X ", " X ", " _ ", " _ ", " _ "],
+            [" _ ", " _ ", " _ ", " _ ", " _ ", " _ ", " _ "],
+            [" _ ", " _ ", " _ ", " _ ", " _ ", " _ ", " _ "],
+            [" _ ", " _ ", " _ ", " _ ", " _ ", " _ ", " _ "],
+            [" _ ", " _ ", " _ ", " _ ", " _ ", " _ ", " _ "]]
+            result = board.check_win("X")
+            expect(result).to be true
+        end
+
+        it "column win" do
+            board = Board.new
+            board.board = [[" _ ", " X ", " X ", " X ", " _ ", " _ ", " _ "],
+            [" X ", " X ", " X ", " _ ", " _ ", " _ ", " _ "],
+            [" X ", " _ ", " _ ", " _ ", " _ ", " _ ", " _ "],
+            [" X ", " _ ", " _ ", " _ ", " _ ", " _ ", " _ "],
+            [" X ", " _ ", " _ ", " _ ", " _ ", " _ ", " _ "],
+            [" _ ", " _ ", " _ ", " _ ", " _ ", " _ ", " _ "]]
+            result = board.check_win("X")
+            expect(result).to be true
+        end
+        it "column win other column" do
+            board = Board.new
+            board.board = [[" _ ", " X ", " X ", " X ", " _ ", " _ ", " _ "],
+            [" X ", " X ", " X ", " _ ", " _ ", " _ ", " _ "],
+            [" X ", " _ ", " _ ", " _ ", " _ ", " _ ", " X "],
+            [" X ", " _ ", " _ ", " _ ", " _ ", " _ ", " X "],
+            [" _ ", " _ ", " _ ", " _ ", " _ ", " _ ", " X "],
+            [" _ ", " _ ", " _ ", " _ ", " _ ", " _ ", " X "]]
+            result = board.check_win("X")
+            expect(result).to be true
+        end
+        it "diagonal win to the right" do
+            board = Board.new
+            board.board = [[" X ", " X ", " _ ", " _ ", " _ ", " _ ", " _ "],
+                           [" X ", " X ", " X ", " _ ", " _ ", " _ ", " _ "],
+                           [" X ", " _ ", " X ", " _ ", " _ ", " _ ", " X "],
+                           [" _ ", " _ ", " _ ", " X ", " _ ", " _ ", " _ "],
+                           [" _ ", " _ ", " _ ", " _ ", " _ ", " _ ", " _ "],
+                           [" _ ", " _ ", " _ ", " _ ", " _ ", " _ ", " X "]]
+            result = board.check_win("X")
+            expect(result).to be true
+        end
+        it "more diagonal win to the right" do
+            board = Board.new
+            board.board = [[" X ", " X ", " _ ", " _ ", " _ ", " _ ", " _ "],
+                           [" X ", " _ ", " X ", " _ ", " _ ", " _ ", " _ "],
+                           [" X ", " _ ", " _ ", " _ ", " _ ", " _ ", " _ "],
+                           [" _ ", " X ", " _ ", " X ", " _ ", " _ ", " _ "],
+                           [" _ ", " _ ", " X ", " _ ", " _ ", " _ ", " _ "],
+                           [" _ ", " _ ", " _ ", " X ", " _ ", " _ ", " _ "]]
+            result = board.check_win("X")
+            expect(result).to be true
+        end
+        it "even more diagonal win to the right" do
+            board = Board.new
+            board.board = [[" X ", " X ", " _ ", " X ", " _ ", " _ ", " _ "],
+                           [" X ", " X ", " X ", " _ ", " X ", " _ ", " _ "],
+                           [" X ", " _ ", " _ ", " _ ", " _ ", " X ", " _ "],
+                           [" _ ", " _ ", " _ ", " _ ", " _ ", " _ ", " X "],
+                           [" _ ", " _ ", " _ ", " _ ", " _ ", " _ ", " _ "],
+                           [" _ ", " _ ", " _ ", " _ ", " _ ", " _ ", " _ "]]
+            result = board.check_win("X")
+            expect(result).to be true
+        end
+    end
+end
+
+describe Player do
+    describe "#update_marker" do
+        it "changes marker from X to O" do
+            me = Player.new("Hunter")
+            me.update_marker("O")
+            marker = me.marker
+            expect(marker).to eq("O")
+        end
+    end
+end
+
+describe Game do
+    
+end
