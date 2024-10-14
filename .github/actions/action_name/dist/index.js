3aa41298231740c9f3619dfd97f41696c0092b44
Hunter-Kendall <hunter.kendall@rolemodelsoftware.com>
2024-10-14 17:28:56 -0400

    labels

--- .github/actions/action_name/dist/index.js
+++ .github/actions/action_name/dist/index.js
@@ -23,21 +23,22 @@ await exec('git checkout staging')
 await exec('git reset --hard origin/main')
 
 for (const pr of pullRequests.data) {
-  const { title, number, head: { ref: branch } }= pr
-  try {
-    await exec('git', ['merge', `origin/${branch}`, '--squash'])
-    await exec('git', ['commit', '-m', `${title} (#${number})`])
-  } catch {
-    await exec('git restore --staged .')
-    await exec('git restore .')
-    await exec('git clean -df')
-    octokit.rest.issues.createComment({
-      owner: github.context.repo.owner,
-      repo: github.context.repo.repo,
-      issue_number: number,
-      body: 'This branch could not be automatically added to staging due to merge conflicts.'
-    })
+  const { title, number, labels, head: { ref: branch } } = pr
+  if (labels.some(label => label.name.toLowerCase() === 'staging')) {
+    try {
+      await exec('git', ['merge', `origin/${branch}`, '--squash'])
+      await exec('git', ['commit', '-m', `${title} (#${number})`])
+    } catch {
+      await exec('git restore --staged .')
+      await exec('git restore .')
+      await exec('git clean -df')
+      octokit.rest.issues.createComment({
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        issue_number: number,
+        body: 'This branch could not be automatically added to staging due to merge conflicts.'
+      })
+    }
   }
-
 }
 await exec('git push --force')
