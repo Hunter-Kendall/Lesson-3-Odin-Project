2884c7dbc9d3a59f0c16b76c422f09062b7b6fed
Hunter-Kendall <hunter.kendall@rolemodelsoftware.com>
2024-10-14 16:12:37 -0400

    try auto merging

--- .github/actions/action_name/dist/index.js
+++ .github/actions/action_name/dist/index.js
@@ -1,11 +1,29 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
+import { exec } from '@actions/exec'
 
 const token = core.getInput('github-token', { required: true })
 const octokit = github.getOctokit(token)
-octokit.rest.issues.createComment({
-  owner: github.context.repo.owner,
-  repo: github.context.repo.repo,
-  issue_number: 1,
-  body: 'This PR sucks!'
+// octokit.rest.issues.createComment({
+//   owner: github.context.repo.owner,
+//   repo: github.context.repo.repo,
+//   issue_number: 1,
+//   body: 'This PR sucks!'
+// })
+
+const pullRequests = await octokit.rest.pulls.list({
+  ...github.context.repo,
+  state: 'open',
+  sort: 'created',
+  direction: 'asc',
 })
+
+await exec('git checkout staging')
+await exec('git reset --hard origin/main')
+
+for (const pr of pullRequests.data) {
+  const { title, number, head: { ref: branch } }= pr
+  await exec('git', ['merge', branch, '--squash'])
+  await exec('git', ['commit', '-m', `${title} (#${number})`])
+}
+await exec('git push --force')
